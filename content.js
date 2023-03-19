18f48db30dd5fa6a9af4dc34de309b98577dd711
tuukkaio <128314510+tuukkaio@users.noreply.github.com>
2023-03-19 14:44:06 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "getZipCode") {
-    const currentUrl = window.location.href;
-    const zipCode = currentUrl.match(/\b\d{5}\b/)[0];
+    const zipCodeElement = document.querySelector("[data-testid='home-details-summary'] div:last-child");
+    const zipCodeText = zipCodeElement ? zipCodeElement.innerText : null;
+    const zipCodeMatch = zipCodeText.match(/\b\d{5}\b/);
+    const zipCode = zipCodeMatch ? zipCodeMatch[0] : null;
     sendResponse({ zipCode });
   }
 });
